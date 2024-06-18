e52dfdba2857da858bbb1081c8671d7eef8ad0ad
befu12 <kiti002718@gmail.com>
2024-06-18 14:51:22 +0900

    userのモデル単体テストコードを実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    name_first            { '山田' }
+    name_last             { '杏奈' }
+    name_first_reading    { 'ヤマダ' }
+    name_last_reading     { 'アンナ' }
+    birthday              {'2000-09-28'}
+  end  
+end  
