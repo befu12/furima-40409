168b15a00b6dbca2cdba2ced570c0068373bd504
befu12 <kiti002718@gmail.com>
2024-07-18 21:51:51 +0900

    修正箇所３

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -14,7 +14,7 @@ class ItemOrder
   end  
 
   def save
-    order = Order.create(item_id: :item_id.to_i, user_id: user_id)
+    order = Order.create(item_id: :item_id, user_id: user_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_num: phone_num, order_id: order.id)
   end
 end  
